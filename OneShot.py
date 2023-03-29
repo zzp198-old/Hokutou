e3b0f276de77b9010b697b155bc805b0c94b8243
zzp198 <zzp198@163.com>
2023-03-29 22:08:23 +0800

    init

---
+++ OneShot.py
@@ -0,0 +1,15 @@
+from sympy import *
+
+CD = Symbol('CD')
+
+result = solve(0.159 * CD + 0.03 * CD * 1.77 - 100, [CD])
+print('不计算克制,猫球加成3%的情况下一刀秒所需爆伤:', result)
+
+result = solve((0.159 * CD + 0.03 * CD * 1.77) * 1.1 - 100, [CD])
+print('计算克制,猫球加成3%的情况下一刀秒所需爆伤:', result)
+
+result = solve(0.159 * CD + 0.03 * CD * 1.77 - 50, [CD])
+print('不计算克制,猫球加成3%的情况下一刀半血所需爆伤:', result)
+
+result = solve((0.159 * CD + 0.03 * CD * 1.77) * 1.1 - 50, [CD])
+print('计算克制,猫球加成3%的情况下一刀半血所需爆伤:', result)
